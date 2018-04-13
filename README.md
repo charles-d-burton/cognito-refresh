f6cbead1f4412e5bec5e38012be041a453317ca1
charles-d-burton <charles.burton@mindflash.com>
2018-04-13 13:16:26 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Cognito Refresh
+
+## Lambda function to refresh Cognito Auth tokens
+
+This function handles exchanging the Auth flow token for valid IDP tokens from AWS Cognito
+
+The request structure to send to the function looks like this:
+
+```json
+{
+    "refresh_token": "some token here"
+}
+```
+
+The response from the services should be either this:
+
+```json
+{
+    "idToken": "the generated token",
+    "accessToken": "the generated access token"
+}
+```
+
+Or if the request is invalid will return a 400 status code.  This means the user needs to sign in again
+
+## Building
+Install Godep for dependency management
+
+```shell
+dep ensure
+GOOS=linux go build -o main
+zip deployment.zip main
+```
+
+TODO:
+ * add more api gateway types
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	idp "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
+)
+
+type refreshToken struct {
+	RefreshToken string `json:"refresh_token"`
+}
+
+type authToken struct {
+	IDToken     string `json:"idToken"`
+	AccessToken string `json:"accessToken"`
+}
+
+//Contact Cognito and exchange refresh_token for idtoken and accesstoken
+func (refToken *refreshToken) refresh() (*idp.InitiateAuthOutput, error) {
+	sess, err := session.NewSession()
+	if err != nil {
+		return nil, err
+	}
+	svc := idp.New(sess)
+	return svc.InitiateAuth(
+		&idp.InitiateAuthInput{
+			AuthFlow:       aws.String("REFRESH_TOKEN"),
+			AuthParameters: map[string]*string{"REFRESH_TOKEN": aws.String(refToken.RefreshToken)},
+			ClientId:       aws.String(os.Getenv("CLIENTID")),
+		},
+	)
+}
+
+//process the APIGateway proxy request and return either an error or exchanged tokens
+func handleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var myRefreshToken refreshToken
+	err := json.Unmarshal([]byte(event.Body), myRefreshToken)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 400}, nil
+	}
+	output, err := myRefreshToken.refresh()
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 400}, nil
+	}
+	var returnToken authToken
+	idToken := *output.AuthenticationResult.IdToken
+	accessToken := *output.AuthenticationResult.AccessToken
+	returnToken.IDToken = idToken
+	returnToken.AccessToken = accessToken
+	data, err := json.Marshal(returnToken)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 400}, nil
+	}
+	return events.APIGatewayProxyResponse{Body: string(data), StatusCode: 200}, nil
+}
+
+//Entrypoint lambda to run code
+func main() {
+	lambda.Start(handleRequest)
+}
